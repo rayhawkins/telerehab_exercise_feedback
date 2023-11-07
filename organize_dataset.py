d9c5caa43b8bccf4e7c00038831c5cb75a7f1e73
rayhawkins <raymond.hawkins@mail.utoronto.ca>
2023-11-07 11:32:23 -0500

    Fixed bug in organize_dataset.py that would not create videos correctly if resizing wasn't happening

--- organize_dataset.py
+++ organize_dataset.py
@@ -33,12 +33,12 @@ from tqdm import tqdm
 import cv2
 from scipy.ndimage import zoom
 
-parent_folder = r"C:\Users\rfgla\Documents\Ray\telerehab_exercise_feedback\data\video_data"
-save_folder = r"C:\Users\rfgla\Documents\Ray\telerehab_exercise_feedback\data\gesture_sorted_data_resized"
+parent_folder = r"C:\Users\Ray\Documents\MASc\BME1570\Data\og_data"
+save_folder = r"C:\Users\Ray\Documents\MASc\BME1570\Data\symmetry_sorted_data"
 perc_test = 10
-max_frames = 16
-group_by_symmetry = False
-rescale_size = (16, 200, 200, 3)  # (t, h, w, c)
+max_frames = None
+group_by_symmetry = True
+rescale_size = None  # (16, 200, 200, 3)  # (t, h, w, c)
 fps = 5  # fps for saving, fps of the original dataset collected from the kinect sensor was 30 fps
 
 np.random.seed(1234)
@@ -99,13 +99,16 @@ for subfolder in os.listdir(parent_folder):
         # Resize if requested
         if rescale_size is not None:  # Rescaling is VERY slow this way
             imgs = zoom(imgs, [rescale_size[i]/imgs.shape[i] for i in range(len(rescale_size))], mode='nearest')
+            video = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, [imgs.shape[1], imgs.shape[2]])
+        else:
+            video = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, [imgs.shape[2], imgs.shape[1]])
 
         if max_frames is not None:
             stop_frame = min(max_frames, len(imgs))
         else:
             stop_frame = len(imgs)
 
-        video = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, [imgs.shape[1], imgs.shape[2]])
+        video = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, [imgs.shape[2], imgs.shape[1]])
         for img in imgs:
             video.write(img)
         video.release()

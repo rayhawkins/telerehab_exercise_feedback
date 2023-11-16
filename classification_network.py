540846b4481057660c82894f382b80263eb73fec
ncopiaev <naomi.opiaevans@gmail.com>
2023-11-15 20:17:10 -0500

    2D CNN

--- classification_network.py
+++ classification_network.py
@@ -2,26 +2,25 @@ import torch
 import torch.nn as nn
 from torch.nn import flatten
 import torch.optim as optim
-
+import argparse
 import sys
 sys.path.append('/Users/owner/Documents/BME Fall2023/BME1570/Assignment 3/telerehab_exercise_feedback/VideoGPT-master')
 from videogpt.vqvae import VQVAE
-import argparse
 
-class classification_network(nn.Module):
+
+class Classifier2(nn.Module):
     def __init__(self, args):
+        self.args = args
 
-        # Load VQ-VAE and set all parameters to no grad
-        self.vqvae = VQVAE.load_from_checkpoint(classes.vqvae)
+    # Load VQ-VAE and set all parameters to no grad
+        self.vqvae = VQVAE.load_from_checkpoint(args.vqvae)
         for p in self.vqvae.parameters():
             p.requires_grad = False
         self.vqvae.codebook._need_init = False
         self.vqvae.eval()
 
-        super(classification_network, self).__init__(in_channels, classes)
-
-
-        # Transformer classifier
+        super(Classifier2, self).__init__(in_channels, classes)
+    # Transformer classifier
         self.in_channels = self.vqvae.in_channels
 
         # First convolutional layer
@@ -34,11 +33,11 @@ class classification_network(nn.Module):
         self.relu2 = nn.ReLU()
         self.maxpool2 = nn.MaxPool2d(kernel_size=self.args.kernel_size)
         # Linear classifier
-        self.linear1 = nn.Linear(in_features=self.args.in_features, out_features=self.classes)
+        self.FC1 = nn.Linear(in_features=self.args.in_features, out_features=classes)
         self.relu3 = nn.ReLU()
 
         # Softmax classifier
-        self.linear2 = nn.Linear(in_features=self.args.in_features, out_features=self.classes)
+        self.FC2 = nn.Linear(in_features=self.args.in_features, out_features=classes)
         self.logSoftmax = nn.LogSoftmax(dim=1)
 
         self.save_hyperparameters()
@@ -55,13 +54,13 @@ class classification_network(nn.Module):
         z = self.maxpool2(z)
 
         # flatten the output from the previous layer and pass it
-        z = flatten(z, self.args.flatten)
-        z = self.Linear1(z)
+        z = flatten(z, 1)
+        z = self.FC1(z)
         z = self.relu3(z)
 
         # Pass output through the softmax classifier
 
-        z = self.Linear2(z)
+        z = self.FC2(z)
         z = self.logSoftmax(z)
 
         return z
@@ -76,11 +75,11 @@ class classification_network(nn.Module):
         parser.add_argument('--out_channels', type=int, default=8)
         parser.add_argument('--in_features', type=int, default=2)
         parser.add_argument('--out_features', type=int, default=2)
-        parser.add_argument('--kernel_size', type=int, default=576)
+        parser.add_argument('--kernel_size', type=tuple, default=(3,3))
         parser.add_argument('--lr', type=float, default=3e-4)
 
 # Define loss function and optimizer
-def configure_optimizer(self):
+def loss_optimizer(self):
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.Adam(self.parameters(), lr=self.args.lr, betas=(0.9, 0.999))
     return loss_function, optimizer
